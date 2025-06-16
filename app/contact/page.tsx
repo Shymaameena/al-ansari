61c47a07dea28d8fa610207d942b1d2b0bbdd6a4
Amjuz <amjupm3@gmail.com>
2025-06-17 02:07:38 +0530

    Chore: Added contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,26 @@
+import BannerSection from "@/components/banner-section";
+import Bounded from "@/components/bounded";
+import PillLabel from "@/components/PillLabel";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function contact() {
   return (
-    <div className="flex justify-center items-center h-screen text-5xl font-bold">
-      Contact Page
+    <div className="">
+      <BannerSection title="Contact Us" />
+      <Bounded className="grid md:grid-cols-2 gap-2">
+        <div className="">
+          <PillLabel className="" label="Contact Us" />
+          <h2 className="">If You Have Any Query, Please Contact Us</h2>
+          <Input placeholder="Your name" />
+          <Input placeholder="Phone number" />
+          <Input placeholder="Email" />
+          <Textarea placeholder="Message" />
+          <Button>Send</Button>
+        </div>
+        <div className="h-full bg-muted-foreground border"></div>
+      </Bounded>
     </div>
-  )
+  );
 }
